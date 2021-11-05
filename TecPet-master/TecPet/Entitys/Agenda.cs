b4c5d3d3d2d089972d8d564aa6d0dbd4a951e1b3
Camila Felix <camila-souza_97@hotmail.com>
2021-11-04 22:49:22 -0300

    Projeto

---
+++ TecPet-master/TecPet/Entitys/Agenda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecPet.Entitys
+{
+    class Agenda
+    {
+        public DateTime DataeHora {get; set;}
+        public float QntRacao {get; set;}
+
+        public Agenda (DateTime dataehora, float qntracao)
+        {
+            DataeHora = dataehora;
+            QntRacao = qntracao;
+           
+
+        }
+
+    }
+}
---
+++ TecPet-master/TecPet/Entitys/Recipiente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecPet.Entitys
+{
+    class Recipiente
+    {
+        public double Peso {get; set; }
+
+        public Recipiente (double peso)
+        {
+            Peso = peso;
+        }
+    }
+
+
+}
