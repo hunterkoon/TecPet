775c84e9a6c5e013447fda2819146ebf78115b6b
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-11-05 13:56:42 -0300

    RepositoryModel s

---
+++ TecPet/Repository/BaseConection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using TecPet.Model;
+
+namespace TecPet.Repository
+{
+   public class BaseConection : Controle
+    {
+      
+
+        public Controle GetDados (Controle dados)
+        {
+
+         string MyStringConnnection = "Server=localhost;Port=3306;database=creche;user id=root;password=Data@071194";
+
+            using (IDbConnection connection = new MySqlConnection(MyStringConnnection))
+            {
+                try
+                {
+                    Controle con = new Controle();
+
+                    connection.Open();
+                    var sql = "SELECT * FROM alunos";
+
+                    con = connection.Query<Controle>(sql).FirstOrDefault();
+
+                    connection.Close();
+
+                    return con;
+                }
+                catch
+                {
+                    throw new Exception("Erro na conexão com banco de dados");
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
