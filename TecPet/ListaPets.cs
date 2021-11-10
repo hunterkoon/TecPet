9e1a0de5b57a3fecc18c1442e32dc4f550278357
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-11-10 17:54:17 -0300

    Solved Conflict

--- TecPet/ListaPets.cs
+++ TecPet/ListaPets.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,31 +18,60 @@ namespace TecPet
 
     public partial class ListaPets : Form
     {
-        BaseConection.Repository repository = new BaseConection.Repository();
-        AnimalModel animal = new AnimalModel();
 
+
+        BaseConection.Repository repository = new BaseConection.Repository();
+        List<AnimalModel> Animais = new List<AnimalModel>();
+        AnimalModel SelectedAnimal = new AnimalModel();
+        UsuarioModel usuario = new UsuarioModel();
 
         public ListaPets()
         {
             InitializeComponent();
+
+
+            try
+            {
+                Animais = repository.GetPets();
+                for (int i = 0; i < Animais.Count(); i++)
+                {
+
+                    tabelaMeusPets.Rows.Add(Animais[i].Id, Animais[i].Nome, Animais[i].Raca, Animais[i].Idade, Animais[i].Peso, Animais[i].Imagem);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
 
         }
 
 
         private void BtnAtualizar_Click(object sender, EventArgs e)
         {
-            tabelaPets();
+
+            try
+            {
+                tabelaMeusPets.Rows.Clear();
+                Animais = repository.GetPets();
+                for (int i = 0; i < Animais.Count(); i++)
+                {
+                    tabelaMeusPets.Rows.Add(Animais[i].Id, Animais[i].Nome, Animais[i].Raca, Animais[i].Idade, Animais[i].Peso);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
         }
 
         private void deleteBtn_Click(object sender, EventArgs e)
         {
-            if (animal.Id != 0)
-            {
-                repository.DeletePet(animal.Id);
-                MessageBox.Show("Pet Deletado");
-            }
-            else
-                MessageBox.Show("Selecione um Pet para deletar");
+
+
         }
 
 
@@ -51,22 +79,18 @@ namespace TecPet
         {
 
 
-            if (tabelaMeusPets.SelectedCells.Count > 0)
-            {
-                int selectedRowIndex = tabelaMeusPets.SelectedCells[0].RowIndex;
-                DataGridViewRow selectedRow = tabelaMeusPets.Rows[selectedRowIndex];
-                int cellValue = Convert.ToInt32(selectedRow.Cells["IdPet"].Value);
-
-                animal.Id = cellValue;
-
-            }
-            else
-                MessageBox.Show("Não selecionado");
-
-
         }
 
         private void welcomeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cadastrarPetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void cadastrarUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
@@ -77,59 +101,5 @@ namespace TecPet
 
         }
 
-        public void tabelaPets()
-        {
-
-            try
-            {
-                tabelaMeusPets.Rows.Clear();
-                var dados = repository.GetPets();
-                for (int i = 0; i < dados.Count(); i++)
-                {
-                    if (dados[i].Imagem != null)
-                    {
-                        // blob to byte
-                        byte[] imageBytes = dados[i].Imagem;
-                        MemoryStream buf = new MemoryStream(imageBytes);
-
-                        // to image 
-                        Image image = Image.FromStream(buf, true);
-
-                        // rotate image
-                        image.RotateFlip(RotateFlipType.Rotate90FlipX);
-
-                        //popular lista
-                        tabelaMeusPets.Rows.Add(dados[i].Id, dados[i].Nome, dados[i].Raca, dados[i].Idade, dados[i].Peso, dados[i].TipoPet,image);
-                        imagemColumn.ImageLayout = DataGridViewImageCellLayout.Stretch;
-                        tabelaMeusPets.Rows[i].Height = 100;                       
-
-                    }
-                    else
-                        tabelaMeusPets.Rows.Add(dados[i].Id, dados[i].Nome, dados[i].Raca, dados[i].Idade, dados[i].Peso, dados[i].TipoPet);
-
-
-                }
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-
-
-        }
-
-        private void cadastrarPetToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            CadastroPet cadPet = new CadastroPet();
-            cadPet.Show();
-
-        }
-
-        private void cadastrarUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            CadastroUsuario cadUser = new CadastroUsuario();
-            cadUser.Show();
-        }
     }
 }
